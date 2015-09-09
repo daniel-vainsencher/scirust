88a5444b6b075d8ae433032ab35c2afe72f4be89
Daniel Vainsencher <daniel.vainsencher@princeton.edu>
2015-09-09 17:47:41 -0400

    Add submatrix copying method.

--- src/matrix/matrix.rs
+++ src/matrix/matrix.rs
@@ -1122,6 +1122,15 @@ impl<T:MagmaBase> Matrix<T> {
         let result : MatrixView<T> = MatrixView::new(self, start_row, start_col, num_rows, num_cols);
         result
     }
+    pub fn submatrix(&self, rows: Vec<usize>, cols: Vec<usize>) -> Matrix<T> {
+        let mut res = Matrix::<T>::new(rows.len(), cols.len());
+        for c in 0..res.num_cols() {
+            for r in 0..res.num_rows() {
+                res.set(r, c, self.get(rows[r], cols[c]).unwrap());
+            }
+        }
+        res
+    }
 }
 
 
